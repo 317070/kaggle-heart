b2002f80e67c0f2266b0fb6b1032f9fd2ee44a46
IraKorshunova <i.hmarynka@gmail.com>
2015-12-17 15:07:56 +0100

    data converter without metadata

--- convert2np.py
+++ convert2np.py
@@ -2,11 +2,34 @@ import os
 import numpy as np
 import dicom
 import cPickle as pickle
+import sys
+from collections import defaultdict
+
+
+def pad_with_zeros(img, out_shape):
+    """pad image with zeros"""
+    in_shape = img.shape
+    out_img = img
+    # rows are equal -> add columns
+    col_diff = out_shape[1] - in_shape[1]
+    if col_diff:
+        pad_img = np.zeros((in_shape[0], col_diff / 2))
+        out_img = np.concatenate((pad_img, out_img, pad_img), axis=1)
+
+    row_diff = out_shape[0] - in_shape[0]
+    if row_diff:
+        pad_img = np.zeros((row_diff / 2, in_shape[1]))
+        out_img = np.concatenate((pad_img, out_img, pad_img))
+
+    assert out_shape == out_img.shape
+    return out_img
 
 
 def read_dicom(filename):
     d = dicom.read_file(filename)
     img = d.pixel_array
+    # print np.sum(np.array(img) - np.array(img, dtype='uint8'))
+    # print np.max(np.array(img)), np.min(np.array(img))
     slice_location = d.SliceLocation
     pixel_spacing = d.PixelSpacing
     slice_thickness = d.SliceThickness
@@ -33,6 +56,7 @@ def convert_view_2np(in_paths, out_path, view):
 
     # final data is a 4d array: (n_slices, n_times, img_height, img_width)
     d_slices = []  # list of slices (for sax)
+    m_slices = []  # metadata per slice
     for s in in_paths:
         n_trials = 1  # for most of the studies
         files = os.listdir(s)
@@ -56,23 +80,41 @@ def convert_view_2np(in_paths, out_path, view):
         offsets.append(offset[0])
         d_slices.append(d_time)
     # check shapes
-    shapes = [dt.shape for ds in d_slices for dt in ds]
-    n_shapes = len(set(shapes))
-    if n_shapes == 1:
-        data = np.array(d_slices, dtype='float32')
-    else:
-        # raise ValueError('dimension mismatch')
-        # TODO: transpose when possible
-        # TODO: rescale when needed
-        print 'image shapes mismatch'
-        print 'study', in_paths[0]
-        print set(shapes)
-        print shapes
-        print '====================================================='
-    print 'view:', view
-    print 'slices:', slices
-    print 'offsets', offsets
-    print 'data shape', data.shape
+    img_shapes = [dt.shape for ds in d_slices for dt in ds]
+    shapes_set = list(set(img_shapes))
+    n_shapes = len(shapes_set)
+    if n_shapes == 2:
+        print 'shapes are different:'
+        print shapes_set
+        # check if it's transposed
+        if shapes_set[0][0] == shapes_set[1][1] and shapes_set[0][1] == shapes_set[1][0]:
+            # select most common shape
+            d = defaultdict(int)
+            for s in img_shapes:
+                d[s] += 1
+            common_shape = sorted(d.keys(), key=d.get)[-1]
+            for i, ds in enumerate(d_slices):
+                for j, dt in enumerate(ds):
+                    if dt.shape != common_shape:
+                        d_slices[i][j] = np.transpose(dt)
+            print 'converted to shape', common_shape, d
+        # or we need to pad zeros
+        else:
+            shapes_sorted = sorted(sorted(shapes_set, key=lambda tup: tup[0]), key=lambda tup: tup[1])
+            largest_shape = shapes_sorted[-1]
+            for i, ds in enumerate(d_slices):
+                for j, dt in enumerate(ds):
+                    if dt.shape != largest_shape:
+                        d_slices[i][j] = pad_with_zeros(dt, largest_shape)
+            print 'converted to shape', largest_shape
+    elif n_shapes > 2:
+        raise ValueError('3 different img shapes!!!!')
+
+    data = np.array(d_slices)
+    # print 'view:', view
+    # print 'slices:', slices
+    # print 'offsets', offsets
+    # print 'data shape', data.shape
     metadata = None  # TODO: add slicing info, offsets and metadata from dicom files
 
     out_filename = out_path + view + '.pkl'
@@ -80,13 +122,16 @@ def convert_view_2np(in_paths, out_path, view):
         pickle.dump({'data': data,
                      'slices': slices,
                      'offsets': offsets,
-                     'metadata': metadata}, f)
-    print 'saved to %s' % out_filename
-    print
+                     'metadata': metadata}, f, protocol=pickle.HIGHEST_PROTOCOL)
+        # print 'saved to %s' % out_filename
+        # print
 
 
 if __name__ == '__main__':
-    global_path = '/mnt/sda3/data/kaggle-heart/'
+    if len(sys.argv) == 2:
+        global_path = sys.argv[1]
+    else:
+        global_path = '/mnt/sda3/data/kaggle-heart/'
     in_data_path = global_path + 'validate/'
     out_data_path = global_path + 'proc_validate/'
 
@@ -97,6 +142,9 @@ if __name__ == '__main__':
     for p in out_study_paths:
         if not os.path.exists(p):
             os.makedirs(p)
+
+    # s = '/mnt/sda3/data/kaggle-heart/validate/643/study/'
+    # convert_study_2np(s, '/mnt/sda3/data/kaggle-heart/proc_validate/663/study/')
 
     for s_in, s_out in zip(in_study_paths, out_study_paths):
         print '******** %s *********' % s_in

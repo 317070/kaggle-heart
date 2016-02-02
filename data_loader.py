some bug fixes, all units are now epochs

data_loader.py
@@ -29,8 +29,8 @@ regular_labels = pickle.load(open("/data/dsb15_pkl/train.pkl","r"))
 
 test_patient_folders = sorted(glob.glob("/data/dsb15_pkl/pkl_validate/*/study/"), key=lambda folder: int(re.search(r'/(\d+)/', folder).group(1)))  # glob is non-deterministic!
 
-NUM_TRAIN_PATIENTS = len(validation_patient_folders)
-NUM_VALID_PATIENTS = len(train_patient_folders)
+NUM_VALID_PATIENTS = len(validation_patient_folders)
+NUM_TRAIN_PATIENTS = len(train_patient_folders)
 NUM_TEST_PATIENTS = len(test_patient_folders)
 
 NUM_PATIENTS = NUM_TRAIN_PATIENTS + NUM_VALID_PATIENTS + NUM_TEST_PATIENTS
@@ -113,9 +113,9 @@ def get_patient_data(indices, wanted_input_tags, wanted_output_tags, set="train"
                 patient_result[tag] = [pickle.load(open(f, "r"))['data'].astype('float32') for f in files if "sax" in f]
             elif tag.startswith("sliced:data:shape"):
                 patient_result[tag] = [pickle.load(open(f, "r"))['data'].shape for f in files]
-            elif tag.startswith("sliced:meta:"):
+            elif tag.startswith("sliced:meta"):
                 # get the key used in the pickle
-                key = tag[len("slided:meta:"):]
+                key = tag[len("slided:meta"):]
                 patient_result[tag] = [pickle.load(open(f, "r"))['metadata'][key] for f in files]
             # add others when needed
 
@@ -219,7 +219,7 @@ def generate_train_batch(required_input_keys, required_output_keys):
     sunny_chunk_size = config().sunny_batch_size * config().batches_per_chunk
     chunk_size = config().batch_size * config().batches_per_chunk
 
-    for n in xrange(config().num_chunks_train):
+    while True:
         result = {}
         input_keys_to_do = list(required_input_keys) #clone
         output_keys_to_do = list(required_output_keys) #clone
@@ -291,7 +291,7 @@ def generate_test_batch(required_input_keys, required_output_keys, augmentation=
     output_keys_to_do = list(required_output_keys) # clone
 
     for set in sets:
-        regular_length = get_lenght_of_set(name="regular", set=set)
+        regular_length = get_lenght_of_set(name="regular", set=set) * config().test_time_augmentations
         num_batches = int(np.ceil(regular_length / float(config().batch_size)))
         regular_chunk_size = config().batches_per_chunk * config().batch_size
         num_chunks = int(np.ceil(num_batches / float(config().batches_per_chunk)))

util_scripts/show_train_plots.py
@@ -5,8 +5,10 @@ import numpy as np
 import cPickle as pickle
 
 print "Looking for the metadata files..."
-files = sorted(glob.glob(os.path.expanduser("~/storage/metadata/kaggle-heart/train/*crps*.pkl")))
+files = sorted(glob.glob(os.path.expanduser("~/storage/metadata/kaggle-heart/train/*slice3*.pkl")))
 print "Plotting..."
+
+NUM_TRAIN_PATIENTS = 417
 
 for file in files:
     try:
@@ -31,8 +33,11 @@ for file in files:
             plt.plot(x_valid, valid_losses)
             x_kaggle = np.arange(0,len(train_losses),1.0*len(train_losses)/len(kaggle_losses))+1
             plt.plot(x_kaggle, kaggle_losses)
-        print "min kaggle loss:", min(kaggle_losses)
-        print "end kaggle loss:", kaggle_losses[-1]
+
+        plt.xlabel("chunks")
+        plt.ylabel("error")
+        print "min kaggle loss:", min(valid_losses)
+        print "end kaggle loss:", valid_losses[-1]
         plt.show()
     except:
         print "%s is corrupt. Skipping" % file
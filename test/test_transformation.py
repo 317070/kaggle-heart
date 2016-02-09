cb1d16bbe6ce91c51c7d2deac08cf5c99cbf3a68
IraKorshunova <i.hmarynka@gmail.com>
2016-02-09 15:21:27 +0100

    test_movies

--- test/test_transformation.py
+++ test/test_transformation.py
@@ -9,7 +9,6 @@ import data_test
 
 pid = 519
 slice_id = 0
-data_path = '/mnt/sda3/data/kaggle-heart/pkl_validate'
 patch_size = (128, 128)
 train_transformation_params = {
     'patch_size': patch_size,
@@ -25,52 +24,47 @@ valid_transformation_params = {
     'shear_range': None
 }
 
-patient_path = data_path + '/%s/study/' % pid
-spaths = sorted(glob.glob(patient_path + '/sax_*.pkl'), key=lambda x: int(re.search(r'/\w*_(\d+)*\.pkl$', x).group(1)))
-
-data = data_test.read_slice(spaths[slice_id])
-metadata = data_test.read_metadata(spaths[slice_id])
-
-print data.shape
-print metadata
-
-
-def init():
-    im.set_data(data[0])
+data_path = '/mnt/sda3/data/kaggle-heart/pkl_validate'
+patient_path = glob.glob(data_path + '/*/study')
+for p in patient_path:
+    print p
+    spaths = sorted(glob.glob(p + '/sax_*.pkl'), key=lambda x: int(re.search(r'/\w*_(\d+)*\.pkl$', x).group(1)))
+    for s in spaths:
+        data = data_test.read_slice(s)
+        metadata = data_test.read_metadata(s)
 
 
-def animate(i):
-    im.set_data(data[i])
-    return im
+        def init():
+            im.set_data(data[0])
 
 
-fig = plt.figure()
-im = fig.gca().imshow(data[0], cmap='gist_gray_r', vmin=0, vmax=255)
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=30, interval=50)
-
-# ---------------------------------
+        def animate(i):
+            im.set_data(data[i])
+            return im
 
 
-out_data = data_test.fix_image_orientation(data, metadata)
-print out_data.shape
+        fig = plt.figure(1)
+        fig.canvas.set_window_title(s)
+        plt.subplot(121)
+        im = plt.gca().imshow(data[0], cmap='gist_gray_r', vmin=0, vmax=255)
+        anim = animation.FuncAnimation(fig, animate, init_func=init, frames=30, interval=50)
 
-# dd = data_test.normalize_contrast(np.copy(data))
-# out_data, pixelspacing = data_test.transform_with_metadata(dd, metadata, valid_transformation_params)
-# print out_data.shape
-# print pixelspacing
+        # ---------------------------------
 
-
-def init_out():
-    im2.set_data(out_data[0])
-
-
-def animate_out(i):
-    im2.set_data(out_data[i])
-    return im2
+        out_data = data_test.fix_image_orientation(data, metadata)
+        out_data = data_test.normalize_contrast(out_data)
 
 
-fig = plt.figure()
-im2 = fig.gca().imshow(out_data[0], cmap='gist_gray_r', vmin=0, vmax=255)
-anim2 = animation.FuncAnimation(fig, animate_out, init_func=init_out, frames=30, interval=50)
+        def init_out():
+            im2.set_data(out_data[0])
 
-plt.show()
+
+        def animate_out(i):
+            im2.set_data(out_data[i])
+            return im2
+
+        plt.subplot(122)
+        im2 = fig.gca().imshow(out_data[0], cmap='gist_gray_r', vmin=0., vmax=1.)
+        anim2 = animation.FuncAnimation(fig, animate_out, init_func=init_out, frames=30, interval=50)
+
+        plt.show()
